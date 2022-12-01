b94963a6432b3b957b6cd17b3a894abaf4077705
andrey-araujo <andrey_m2@hotmail.com>
2022-12-01 00:01:21 -0300

    #aula09: fixing error when image is not valid and set column alignment

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -29,6 +29,8 @@ class _FormScreenState extends State<FormScreen> {
             border: Border.all(width: 3),
           ),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Padding(
                 padding: const EdgeInsets.all(8.0),
@@ -85,6 +87,9 @@ class _FormScreenState extends State<FormScreen> {
                   borderRadius: BorderRadius.circular(10),
                   child: Image.network(
                     imageController.text,
+                    errorBuilder: (context, error, stackTrace) {
+                      return Image.asset('assets/images/nophoto.png');
+                    },
                     fit: BoxFit.cover,
                   ),
                 ),
